6c6e31917fc0d5a1a205e8893cbe5c7b3a2778e2
002 <002@192.168.1.82>
2018-06-01 16:07:49 +0900

    switch文内を変更

--- src/controller/LoginServlet.java
+++ src/controller/LoginServlet.java
@@ -35,25 +35,20 @@ public class LoginServlet extends HttpServlet {
 		String loginPass = request.getParameter("loginPass");
 
 		// loginIdとloginPassの正規化チェック 半角英数字、ハイフン、アンダースコアのみ許可
-		// 両方ともtrueのときにログイン処理を続行
 		if(loginId.matches("[0-9a-zA-Z\\-\\_]+") && loginPass.matches("[0-9a-zA-Z\\-\\_]+")) {
 			// データベースから該当ユーザーの情報を取得
-			// 該当ユーザーがいない場合又はパスワード間違いの場合、取得失敗、ログインページへ遷移(エラー付き)
 			// 取得成功ならセッションにユーザーデータの一部を記録して"eventlist"サーブレットへ遷移
 			try {
 				UsersDao UsersDao = DaoFactory.createUsersDao();
 				Users user = UsersDao.findByLoginIdAndLoginPass(loginId,  loginPass);
 				String pageName = "eventToday";
 				if(user != null) {
-					// セッションにユーザー情報"loginId"を登録
+					// セッションにユーザー情報"id,loginId,name,type_id"を登録
 					request.getSession().setAttribute("id", user.getId());
-					// セッションにユーザー情報"loginId"を登録
 					request.getSession().setAttribute("loginId", user.getLoginId());
-					//セッションにユーザ名をセット(ヘッダー表示用)
 					request.getSession().setAttribute("name", user.getName());
-					// ユーザー種別"type_id"をセッションに登録, 2なら管理者, 1なら一般
 					request.getSession().setAttribute("type_id", user.getTypeId());
-					request.setAttribute("ServName", pageName);
+					request.setAttribute("servName", pageName);
 					request.getRequestDispatcher("EventServlet").forward(request,  response);
 				} else {
 					request.setAttribute("error", true);
