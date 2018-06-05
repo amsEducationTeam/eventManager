d6b58ef8d1b0fed7f1e83d0933812c473d8f169a
034 <034@192.168.1.80>
2018-06-05 09:29:44 +0900

    保存場所を間違えていたので変更

--- src/domain/Users.java
+++ src/domain/Users.java
@@ -3,38 +3,60 @@ package domain;
 import java.util.Date;
 
 public class Users {
-	private Integer id;
-	private String loginId;
-	private String loginPass;
+	private Integer member_id;
 	private String name;
-	private Integer typeId;
-	private Integer group;
-	private String groupName;
-	private Date created;
+	private String kana;
+	private Integer dep_id;
+	private String address;
+	private String tel;
+	private Date birthday;
+	private Integer position_type;
+	private Date hired;
+	private String login_id;
+	private String department;
+	private Integer floor;
+	private String login_pass;
+	private Integer auth_id;
+	private String auth_type;
 
-	public Integer getId() {
-		return id;
+
+
+
+	public String getLogin_pass() {
+		return login_pass;
 	}
-	public void setId(Integer id) {
-		this.id = id;
+	public void setLogin_pass(String login_pass) {
+		this.login_pass = login_pass;
 	}
-	public String getLoginId() {
-		return loginId;
+	public Integer getAuth_id() {
+		return auth_id;
 	}
-	public void setLoginId(String loginId) {
-		this.loginId = loginId;
+	public void setAuth_id(Integer auth_id) {
+		this.auth_id = auth_id;
 	}
-	public String getLoginPass() {
-		return loginPass;
+	public String getAuth_type() {
+		return auth_type;
 	}
-	public void setLoginPass(String loginPass) {
-		this.loginPass = loginPass;
+	public void setAuth_type(String auth_type) {
+		this.auth_type = auth_type;
 	}
-	public Integer getTypeId() {
-		return typeId;
+	public String getDepartment() {
+		return department;
 	}
-	public void setTypeId(Integer typeId) {
-		this.typeId = typeId;
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public Integer getFloor() {
+		return floor;
+	}
+	public void setFloor(Integer floor) {
+		this.floor = floor;
+	}
+	public Integer getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(Integer member_id) {
+		this.member_id = member_id;
 	}
 	public String getName() {
 		return name;
@@ -42,23 +64,55 @@ public class Users {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Integer getGroup() {
-		return group;
+	public String getKana() {
+		return kana;
 	}
-	public void setGroup(Integer group) {
-		this.group = group;
+	public void setKana(String kana) {
+		this.kana = kana;
 	}
-	public Date getCreated() {
-		return created;
+	public Integer getDep_id() {
+		return dep_id;
 	}
-	public void setCreated(Date created) {
-		this.created = created;
+	public void setDep_id(Integer dep_id) {
+		this.dep_id = dep_id;
 	}
-	public String getGroupName() {
-		return groupName;
+	public String getAddress() {
+		return address;
 	}
-	public void setGroupName(String groupName) {
-		this.groupName = groupName;
+	public void setAddress(String address) {
+		this.address = address;
 	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public Integer getPosition_type() {
+		return position_type;
+	}
+	public void setPosition_type(Integer position_type) {
+		this.position_type = position_type;
+	}
+	public Date getHired() {
+		return hired;
+	}
+	public void setHired(Date hired) {
+		this.hired = hired;
+	}
+	public String getLogin_id() {
+		return login_id;
+	}
+	public void setLogin_id(String login_id) {
+		this.login_id = login_id;
+	}
+
+
 
 }
