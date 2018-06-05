839b743a2ebde2d921e7031a0f2f55dbcb8ba112
002 <002@192.168.1.82>
2018-06-05 13:16:54 +0900

    改行、インデント修正

--- src/dao/EventsDao.java
+++ src/dao/EventsDao.java
@@ -7,25 +7,15 @@ import domain.Events;
 public interface EventsDao {
 
 	List<Events> findAll() throws Exception;
-
 	List<Events> findToday() throws Exception;
-
 	Events findById(Integer id) throws Exception;
-
-	 void insert(Events events) throws Exception;
-
+	void insert(Events events) throws Exception;
 	void update(Events events) throws Exception;
-
-	 void delete(Events events) throws Exception;
-
+	void delete(Events events) throws Exception;
 	List<Events> findAll(int id) throws Exception;
-
 	List<Events> findToday(int id) throws Exception;
-
 	List<Events> findfive(List<Events> eventsList, int id) throws Exception;
-
 	double countAll() throws Exception;
-
 	double countAllToday() throws Exception;
 
 }
