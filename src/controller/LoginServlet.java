584059881fe1357090e21d939d1f390bf91c303d
002 <002@192.168.1.82>
2018-06-05 10:18:52 +0900

    LoginServletのBeens変数名を修正しました

--- src/controller/LoginServlet.java
+++ src/controller/LoginServlet.java
@@ -22,7 +22,6 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		request.getRequestDispatcher("view/login.jsp").forward(request,  response);
 	}
 
@@ -31,24 +30,22 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// requestとresponseからパラメーターの取得
-		String loginId = request.getParameter("loginId");
-		String loginPass = request.getParameter("loginPass");
+
+		String login_id = request.getParameter("login_id");
+		String login_pass = request.getParameter("login_pass");
 
 		// loginIdとloginPassの正規化チェック 半角英数字、ハイフン、アンダースコアのみ許可
-		if(loginId.matches("[0-9a-zA-Z\\-\\_]+") && loginPass.matches("[0-9a-zA-Z\\-\\_]+")) {
-			// データベースから該当ユーザーの情報を取得
-			// 取得成功ならセッションにユーザーデータの一部を記録して"eventlist"サーブレットへ遷移
+		if(login_id.matches("[0-9a-zA-Z\\-\\_]+") && login_pass.matches("[0-9a-zA-Z\\-\\_]+")) {
 			try {
 				UsersDao UsersDao = DaoFactory.createUsersDao();
-				Users user = UsersDao.findByLoginIdAndLoginPass(loginId,  loginPass);
+				Users user = UsersDao.findByLoginIdAndLoginPass(login_id,  login_pass);
 				String pageName = "eventToday";
 				if(user != null) {
 					// セッションにユーザー情報"id,loginId,name,type_id"を登録
-					request.getSession().setAttribute("id", user.getId());
-					request.getSession().setAttribute("loginId", user.getLoginId());
+					request.getSession().setAttribute("member_id", user.getMember_id());
+					request.getSession().setAttribute("login_id", user.getLogin_id());
 					request.getSession().setAttribute("name", user.getName());
-					request.getSession().setAttribute("type_id", user.getTypeId());
+					request.getSession().setAttribute("auth_type", user.getAuth_type());
 					request.setAttribute("servletName", pageName);
 					request.getRequestDispatcher("EventServlet").forward(request,  response);
 				} else {
