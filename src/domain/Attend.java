cebd50cea68e7a71f451ba85f2a49a462940073f
034 <034@192.168.1.80>
2018-06-05 09:14:28 +0900

    attend関連の修正

--- src/domain/Attend.java
+++ src/domain/Attend.java
@@ -1,10 +1,18 @@
 package domain;
 
 public class Attend {
-
+	private int attends_id;
 	private int member_id;
-
+	private int event_id;
 	private String member_name;
+
+	public int getAttends_id() {
+		return attends_id;
+	}
+
+	public void setAttends_id(int attends_id) {
+		this.attends_id = attends_id;
+	}
 
 	public int getMember_id() {
 		return member_id;
@@ -12,6 +20,14 @@ public class Attend {
 
 	public void setMember_id(int member_id) {
 		this.member_id = member_id;
+	}
+
+	public int getEvent_id() {
+		return event_id;
+	}
+
+	public void setEvent_id(int event_id) {
+		this.event_id = event_id;
 	}
 
 	public String getMember_name() {
