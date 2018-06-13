4d35737ace075b68e1729c5a1aacd029bc282976
001 <001@192.168.1.79>
2018-06-13 15:55:22 +0900

    eventTest

--- src/dao/AccountDaoImpl.java
+++ src/dao/AccountDaoImpl.java
@@ -2,9 +2,12 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
+
+import org.mindrot.jbcrypt.BCrypt;
 
 import domain.Account;
 
@@ -21,25 +24,48 @@ public class AccountDaoImpl implements AccountDao {
 	 * @param Account
 	 */
 	@Override
+	// list化
 	public void insertAcount(Account account) throws Exception {
 		try (Connection con = ds.getConnection()) {
+			try {
+				//オートコミットを切る
+				con.setAutoCommit(false);
 
-			// アカウントテーブルに各idとpassをINSERTする
-			String sqlAcc = "INSERT INTO account(login_id,login_pass, auth_id) VALUES(?,?,?);";
-			PreparedStatement stmtAcc = con.prepareStatement(sqlAcc);
-			//account
-			stmtAcc.setString(1, account.getLoginId());
-			stmtAcc.setString(2, account.getLoginPass());
-			stmtAcc.setObject(3, account.getAuthId());
+				// アカウントテーブルに各idとpassをINSERTする
+				String sqlAcc = "INSERT INTO account(login_id,login_pass, auth_id) VALUES(?,?,?);";
+				PreparedStatement stmtAcc = con.prepareStatement(sqlAcc);
+				//account
+				stmtAcc.setString(1, account.getLoginId());
+				String hashPass = BCrypt.hashpw(account.getLoginPass(), BCrypt.gensalt());
+				stmtAcc.setString(2, hashPass);
+				stmtAcc.setObject(3, account.getAuthId());
+				stmtAcc.executeUpdate();
 
-			// メンバテーブルのlogin_idにidをINSERTする
-			String sqlMem = "UPDATE members SET login_id = ? WHERE member_id = ?;";
-			PreparedStatement stmtMem = con.prepareStatement(sqlMem);
-			stmtMem.setString(1, account.getLoginId());
-			stmtMem.setString(2, account.getMemberId());
+				// レコードがあれば更新
+				// members
+				// メンバテーブルのlogin_idにidをINSERTする
+				String sqlMem = "UPDATE members SET login_id = ? WHERE member_id = ?;";
+				PreparedStatement stmtMem = con.prepareStatement(sqlMem);
+				stmtMem.setString(1, account.getLoginId());
+				stmtMem.setString(2, account.getMemberId());
+				stmtMem.executeUpdate();
+			} //挿入時にエラーが発生したらロールバックしてエラー文を表示
+			catch (Exception e) {
+				System.out.println("error1");
+				e.printStackTrace();
+					con.rollback();
+					return;
 
-			stmtAcc.executeUpdate();
-			stmtMem.executeUpdate();
+			} finally {
+				try {
+					if (con != null) {
+						con.close();
+						System.out.println("切断しました");
+					}
+				} catch (SQLException e) {
+					System.out.println("error2");
+				}
+			}
 		}
 	}
 
