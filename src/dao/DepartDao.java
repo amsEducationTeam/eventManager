94e1d20b2e843e9f19bc67720babc0edcace635a
034 <034@192.168.1.80>
2018-06-13 17:00:20 +0900

    部署情報のInsertと役職情報のUpdateを一つのブロックに統合しました。役職情報のUpdateを実行する際にmembersテーブルに存在しないメンバーを指定した場合、部署情報のInsertまで含めてロールバックをするように変更しました。またDepartDaoImplの戻り値をStringのエラーコードに変更しました。

--- src/dao/DepartDao.java
+++ src/dao/DepartDao.java
@@ -8,7 +8,7 @@ import domain.Members;
 
 public interface DepartDao{
 	List<Depart> findAttends(Integer dep_id)throws Exception;
-	void insert(List<Depart> department, int datacounter) throws Exception;
+	String insert(List<Depart> department, int datacounter) throws Exception;
 	void delete(String member_Id, int event_Id) throws Exception;
 	void deleteByUserId(Members user)throws Exception;
 	void deleteByEventId(Events event) throws Exception;
--- src/dao/DepartDaoImpl.java
+++ src/dao/DepartDaoImpl.java
@@ -1,11 +1,15 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
+
+import com.mysql.jdbc.Statement;
 
 import domain.Depart;
 import domain.Events;
@@ -25,12 +29,12 @@ public class DepartDaoImpl implements DepartDao {
 		return null;
 	}
 
-	public void insert(List<Depart> department, int datacounter) throws Exception {
+	public String insert(List<Depart> department, int datacounter) throws Exception {
 
 		try (Connection con = ds.getConnection()){
 			//必ず決まっているデータのスタート地点をセット（テーブルによって量、数値が変わります）
 			int data1 = 2;
-			int data2 = 2;
+			//int data2 = 2;
 			//Connection conn = null;
 			try {
 				//DB接続
@@ -39,6 +43,7 @@ public class DepartDaoImpl implements DepartDao {
 
 
 				//departmentテーブルに部署名とフロア情報を挿入
+				//membersテーブルにpositionタイプ（役職情報）を挿入
 				for (int i = 0; i < datacounter; i++) {
 					String sql = "insert into department (dep_id,department,floor)  values (null,?,?)";
 					PreparedStatement stmt = con.prepareStatement(sql);
@@ -46,41 +51,80 @@ public class DepartDaoImpl implements DepartDao {
 					stmt.setObject(2, department.get(data1).getFloor());
 					System.out.println(stmt);
 					stmt.executeUpdate();
+
+					//member_idを検索して結果をResultSetにセットする
+					String sql1 = "select member_id from members where member_id = ?";
+					PreparedStatement stmt1 = con.prepareStatement(sql1);
+					stmt1.setObject(1, department.get(data1).getPosition_type());
+					ResultSet rs = stmt1.executeQuery();
+
+					//検索結果がnullならロールバック
+					if(rs==null) {
+						con.rollback();
+						return "302";
+					}else {
+
+					//membersテーブルにpositionタイプ（役職情報）を挿入
+					String sql2 = "update Members set position_type =1 where member_id=?;";
+					PreparedStatement stmt2 = con.prepareStatement(sql2);
+					stmt2.setObject(1,department.get(data1).getPosition_type());
+					System.out.println(stmt2);
+					stmt2.executeUpdate();
+					}
 					//次のデータの位置にインデックスを変える（テーブルによって量、数値が変わります）
 					data1++;
 				}
 
-				//membersテーブルにpositionタイプ（役職情報）を挿入
-				for (int i = 0; i < datacounter; i++) {
-					String sql = "update Members set position_type =1 where member_id=?;";
-					PreparedStatement stmt = con.prepareStatement(sql);
-
-					stmt.setObject(1,department.get(data2).getPosition_type());
-					System.out.println(stmt);
-					stmt.executeUpdate();
-					//次のデータの位置にインデックスを変える（テーブルによって量、数値が変わります）
-					data2++;
-				}
 				//エラーがなければコミットする
 				con.commit();
-			} //挿入時にエラーが発生したらロールバックしてエラー文を表示
+				return "100";
+			}
+			//挿入時にエラーが発生したらロールバックしてエラー文を表示
 			catch (Exception e) {
 				System.out.println("error1");
 				e.printStackTrace();
 					con.rollback();
-					return;
+					return "300";
 
 			} finally {
 				try {
 					if (con != null) {
 						con.close();
 						System.out.println("切断しました");
+
 					}
 				} catch (SQLException e) {
 					System.out.println("error2");
 				}
 			}
 		}
+	}
+
+	public static boolean checkDataBassExit(String str, String tableName, String calamName) throws Exception {
+		boolean result=false;
+		int count = 0;
+
+		try(Connection conn=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/eventdb2?useUnicode=true&characterEncoding=utf8&useSSL=false", "root",
+				"rootpass")) {
+			Class.forName("com.mysql.jdbc.Driver");
+			String sql=	" SELECT "
+					+  " COUNT(*) FROM ";
+			sql += tableName + " WHERE ";
+			sql += tableName + "." + calamName;
+			sql += " = '" + str + "';";
+			Statement stmt=(Statement)conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				count = Integer.parseInt(rs.getString("count(*)"));
+			}
+			if(count > 0) {
+				result = true;
+			}
+		} catch (Exception e) {
+			result = false;
+			throw  new Exception(e);
+		}
+		return result;
 	}
 
 	@Override
