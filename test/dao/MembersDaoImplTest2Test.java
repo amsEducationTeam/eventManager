09c051d480fcfcda908bc0327244906b44263a15
033 <033@192.168.1.81>
2018-06-18 14:24:54 +0900

    送ってなかったテストコードを送信します

---
+++ test/dao/MembersDaoImplTest2Test.java
@@ -0,0 +1,19 @@
+package dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MembersDaoImplTest2Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
