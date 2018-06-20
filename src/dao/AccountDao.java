d75cf707c1655d577fdcdaf79304e3256cdc41aa
002 <002@192.168.1.82>
2018-06-20 15:59:01 +0900

    AccountDaoImplのテストを作成しました

--- src/dao/AccountDao.java
+++ src/dao/AccountDao.java
@@ -5,7 +5,5 @@ import java.util.List;
 import domain.Account;
 
 public interface AccountDao{
-	List<Account> findAll()throws Exception;
-
 	public String insertAcount(List<Account> accountList) throws Exception;
 }
