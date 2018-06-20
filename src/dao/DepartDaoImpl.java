3ff6f4a13367fed95fe756c68b8c97f4664691d6
034 <034@192.168.1.80>
2018-06-20 13:40:29 +0900

    エラーコードをコンソール表示から戻り値へ変更しました。

--- src/dao/DepartDaoImpl.java
+++ src/dao/DepartDaoImpl.java
@@ -19,7 +19,6 @@ public class DepartDaoImpl implements DepartDao {
 		this.ds = ds;
 	}
 
-
 	@Override
 	public List<Depart> findAttends(Integer dep_id) throws Exception {
 		// TODO 自動生成されたメソッド・スタブ
@@ -28,7 +27,7 @@ public class DepartDaoImpl implements DepartDao {
 
 	public String insert(List<Depart> department, int datacounter) throws Exception {
 
-		try (Connection con = ds.getConnection()){
+		try (Connection con = ds.getConnection()) {
 			//必ず決まっているデータのスタート地点をセット（テーブルによって量、数値が変わります）
 			int data1 = 0;
 			//int data2 = 2;
@@ -38,11 +37,9 @@ public class DepartDaoImpl implements DepartDao {
 				//オートコミットを切る
 				con.setAutoCommit(false);
 
-
 				//departmentテーブルに部署名とフロア情報を挿入
 				//membersテーブルにpositionタイプ（役職情報）を挿入
 				for (int i = 0; i < datacounter; i++) {
-
 
 					String sql = "insert into department (dep_id,department,floor)  values (null,?,?)";
 					PreparedStatement stmt = con.prepareStatement(sql);
@@ -57,23 +54,23 @@ public class DepartDaoImpl implements DepartDao {
 					PreparedStatement stmt1 = con.prepareStatement(sql1);
 					stmt1.setObject(1, department.get(data1).getPosition_type());
 					ResultSet rs = stmt1.executeQuery();
-					int member_count=0;
+					int member_count = 0;
 					while (rs.next()) {
 						member_count = Integer.parseInt(rs.getString("count(*)"));
 					}
 
 					//検索結果が0ならロールバック
-					if(member_count==0) {
+					if (member_count == 0) {
 						con.rollback();
 						return "302";
-					}else {
+					} else {
 
-					//membersテーブルにpositionタイプ（役職情報）を挿入
-					String sql2 = "update Members set position_type =1 where member_id=?;";
-					PreparedStatement stmt2 = con.prepareStatement(sql2);
-					stmt2.setObject(1,department.get(data1).getPosition_type());
-					System.out.println(stmt2);
-					stmt2.executeUpdate();
+						//membersテーブルにpositionタイプ（役職情報）を挿入
+						String sql2 = "update Members set position_type =1 where member_id=?;";
+						PreparedStatement stmt2 = con.prepareStatement(sql2);
+						stmt2.setObject(1, department.get(data1).getPosition_type());
+						System.out.println(stmt2);
+						stmt2.executeUpdate();
 					}
 					//次のデータの位置にインデックスを変える（テーブルによって量、数値が変わります）
 					data1++;
@@ -85,25 +82,22 @@ public class DepartDaoImpl implements DepartDao {
 			}
 			//挿入時にエラーが発生したらロールバックしてエラー文を表示
 			catch (Exception e) {
-				System.out.println("error1");
 				e.printStackTrace();
-					con.rollback();
-					return "300";
+				con.rollback();
+				return "300";
 
 			} finally {
 				try {
 					if (con != null) {
 						con.close();
-						System.out.println("切断しました");
 
 					}
 				} catch (SQLException e) {
-					System.out.println("error2");
+					return "301";
 				}
 			}
 		}
 	}
-
 
 	@Override
 	public void delete(String member_Id, int event_Id) throws Exception {
@@ -122,7 +116,5 @@ public class DepartDaoImpl implements DepartDao {
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
-
-
 
 }
