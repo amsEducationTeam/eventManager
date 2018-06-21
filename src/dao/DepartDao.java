2976e151d0cc1a3f1013c599e9cffdc34dd22bfc
033 <033@192.168.1.81>
2018-06-21 10:19:01 +0900

    コードの変更を行いました

--- src/dao/DepartDao.java
+++ src/dao/DepartDao.java
@@ -3,13 +3,11 @@ package dao;
 import java.util.List;
 
 import domain.Depart;
-import domain.Events;
-import domain.Members;
 
 public interface DepartDao{
-	List<Depart> findAttends(Integer dep_id)throws Exception;
+
 	String insert(List<Depart> department) throws Exception;
-	void delete(String member_Id, int event_Id) throws Exception;
-	void deleteByUserId(Members user)throws Exception;
-	void deleteByEventId(Events event) throws Exception;
+
+	
+	
 }
